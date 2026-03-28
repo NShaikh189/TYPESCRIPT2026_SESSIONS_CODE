2551ceae8995b9df86f2d963645d5f83c4998c79
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-28 23:20:38 +0530

    typescript files

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let msg = "hello world";
+console.log(msg);
+function testing(browser) {
+    console.log(browser.length);
+}
+testing('chrome');
+//# sourceMappingURL=app.js.map
